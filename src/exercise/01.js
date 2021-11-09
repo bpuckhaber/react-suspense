4dd85ba127ff04ec4228c29dc53b2a09adc11666
Ben Puckhaber <ben.puckhaber@gmail.com>
2021-11-09 05:27:46 -0600

    exercise.01-extra-01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,15 +2,21 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {PokemonDataView, fetchPokemon} from '../pokemon'
+import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
 
 let pokemon
+let error
 
-const pokemonPromise = fetchPokemon('pikachu').then(
+const pokemonPromise = fetchPokemon('pikacha').then(
   resolvedValue => (pokemon = resolvedValue),
+  reason => (error = reason),
 )
 
 function PokemonInfo() {
+  if (error) {
+    throw error
+  }
+
   if (!pokemon) {
     throw pokemonPromise
   }
@@ -29,9 +35,11 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        <React.Suspense fallback={'Loading Pokemon...'}>
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={'Loading Pokemon...'}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
